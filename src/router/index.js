735a31f8ed606deb926861b6f7059e79e336429a
taraqjatri <130284178+taraqjatri@users.noreply.github.com>
2023-10-02 17:14:26 +0600

    Update front-end with loading icon

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Index from '../views/Index.vue'
+import About from '../views/About.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,14 +10,11 @@ const router = createRouter({
       name: 'index',
       component: Index
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/about',
+      name: 'about',
+      component: () => About
+    }
   ]
 })
 
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -218,7 +218,8 @@ const isDropDownOpen = ref(false);
 const toggledWeatherOrPlaceNear = ref(true);
 const offset = ref(0);
 const countryId = ref(0);
-const loadingIcon = ref(false);
+const loadingIconForCity = ref(false);
+const loadingIconForDetailsAndPlaces = ref(false);
 
 const filteredCountries = computed(() => {
   return countries.filter((country) => !searchCountry.value || country.toLowerCase().includes(searchCountry.value.toLowerCase()));
@@ -232,12 +233,18 @@ function selectCountry(country) {
 }
 
 function selectCity(city) {
+  loadingIconForDetailsAndPlaces.value = true;
   searchCity.value = city.name;
   cityDetails.value = city;
 
   toggledDropDown();
-  getWeatherDetails(city.latitude.toFixed(2), city.longitude.toFixed(2));
-  getPlacesNearLocation(city);
+
+  setTimeout(() => {
+    getWeatherDetails(city.latitude.toFixed(2), city.longitude.toFixed(2));
+    getPlacesNearLocation(city);
+
+    loadingIconForDetailsAndPlaces.value = false;
+  }, 2000);
 }
 
 async function getCountry(url) {
@@ -254,6 +261,7 @@ async function getCountry(url) {
   };
 
   try {
+    loadingIconForCity.value = true;
     await axios.request(options)
         .then(async (res) => {
           country.value = res.data.data[0];
@@ -262,7 +270,10 @@ async function getCountry(url) {
               try {
                 country.value = res.data.data[0];
                 countryId.value = country.value.wikiDataId;
+
                 await getCities();
+
+                loadingIconForCity.value = false;
               } catch (error) {
                 console.error("Request failed:", error);
                 resolve(null);
@@ -273,6 +284,7 @@ async function getCountry(url) {
         .catch(error => {
           console.log("The error is : ", error);
         });
+    loadingIconForCity.value = false;
   } catch (error) {
     console.error(error);
   }
@@ -282,7 +294,7 @@ async function getCountry(url) {
 async function getCities() {
   const options = {
     method: 'GET',
-    url: "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds="+countryId.value,
+    url: "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds=" + countryId.value,
     params: {
       limit: '10',
       offset: offset.value
@@ -358,7 +370,7 @@ function clearValues() {
   cities.value = '';
 }
 
-function getMoreCities(){
+function getMoreCities() {
   offset.value += 10;
   getCities();
 }
@@ -366,7 +378,7 @@ function getMoreCities(){
 
 <template>
   <title>Weather App</title>
-  <h1 v-if="loadingIcon">Hello</h1>
+
   <div class="h-screen flex items-center justify-center bg-gray-400">
     <div
         class="max-w-sm w-1/3 mx-auto p-8 bg-white rounded-xl shadow-2xl h-full"
@@ -411,11 +423,36 @@ function getMoreCities(){
         </div>
       </div>
 
+      <div v-if="loadingIconForCity">
+        <div class="relative mt-10">
+          <div>
+            <button type="button"
+                    class="w-full gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    id="menu-button" aria-expanded="true" aria-haspopup="true">
+
+              <div role="status" class="flex">
+                <svg aria-hidden="true" class="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                     viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"/>
+                  <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"/>
+                </svg>
+                Loading...
+              </div>
+
+            </button>
+          </div>
+        </div>
+      </div>
+
       <div v-if="cities">
         <div class="relative mt-10">
           <div>
             <button @click="toggledDropDown" type="button"
-                    class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    class="inline-flex w-full p-2 pl-10 gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                     id="menu-button" aria-expanded="true" aria-haspopup="true">
               {{ cityDetails ? cityDetails.name : "Select City" }}
               <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -443,6 +480,21 @@ function getMoreCities(){
 
             </div>
           </div>
+        </div>
+      </div>
+
+      <div v-if="loadingIconForDetailsAndPlaces">
+        <div role="status" class="h-96 flex items-center justify-center">
+          <svg aria-hidden="true" class="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+               viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"/>
+            <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"/>
+          </svg>
+          Loading...
         </div>
       </div>
 
